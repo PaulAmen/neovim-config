ed0b12dc830a786674e88b7d82dc181c4cdf2e3c
Paul Amen <amenster@gmail.com>
2026-04-16 21:07:45 -0500

    feat: actualizar plugins y mejorar configuración de Treesitter
    - Se actualizaron múltiples plugins a sus últimas versiones en lazy-lock.json.
    - Se mejoró la configuración de nvim-treesitter, especificando el branch y reestructurando el archivo para mayor claridad y mantenimiento.

--- lua/paul/plugins/treesitter.lua
+++ lua/paul/plugins/treesitter.lua
@@ -1,14 +1,27 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = { 'javascript', 'typescript', 'json', 'html', 'css', 'lua', 'vim', 'python', 'bash', 'markdown', 'markdown_inline' },
-      highlight = { 
-        enable = true,
-        disable = { "latex", "tex" },
-      },
-      indent = { enable = true },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"javascript",
+				"typescript",
+				"json",
+				"html",
+				"css",
+				"lua",
+				"vim",
+				"python",
+				"bash",
+				"markdown",
+				"markdown_inline",
+			},
+			highlight = {
+				enable = true,
+				disable = { "latex", "tex" },
+			},
+			indent = { enable = true },
+		})
+	end,
 }
